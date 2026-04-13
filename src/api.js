dd1ea4aa30e2572e41f00924e9877d9927fc965e
medialiftadvisor <medialiftadvisor@gmail.com>
2026-04-13 10:02:45 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,1 +1,19 @@
 
+import axios from 'axios';
+
+// Apne Hostinger ka sahi URL yahan dalein
+const API_BASE_URL = 'https://mall.zaminzaydaat.com/admin_api.php';
+
+export const fetchProducts = async (category = 'All', pincode = '') => {
+  try {
+    const response = await axios.post(API_BASE_URL, {
+      action: 'get_products',
+      category,
+      pincode
+    });
+    return response.data.products;
+  } catch (error) {
+    console.error("API Error:", error);
+    return [];
+  }
+};
